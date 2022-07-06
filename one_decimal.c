6e9ce8e91cfd26d9a15e6774343dbbe805a29b88
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-07-06 22:08:02 +0200

    Typo

--- one_decimal.c
+++ one_decimal.c
@@ -14,25 +14,25 @@ int main(void)
     float nearest =one_decimal_rounded_nearest(number);
     float up = one_decimal_rounded_up(number);
 
-    printf("rounded down %.2f\n", down);
-    printf("rounded nearest %.2f\n", nearest);
-    printf("rounded up %.2f\n", up);
+    printf("rounded down %.1f\n", down);
+    printf("rounded nearest %.1f\n", nearest);
+    printf("rounded up %.1f\n", up);
 
     return 0;
 }
 
 float one_decimal_rounded_down(float num)
 {
-    float calc = floorf(num * 100) / 100;
+    float calc = floorf(num * 1000) / 1000;
     return calc;
 }
 float one_decimal_rounded_nearest(float num)
 {
-    float calc = roundf(num * 100) / 100;
+    float calc = roundf(num * 1000) / 1000;
     return calc;
 }
 float one_decimal_rounded_up(float num)
 {
-    float calc = ceilf(num * 100) / 100;
+    float calc = ceilf(num * 1000) / 1000;
     return calc;
 }
